f65602750f54908b642bf4e9dc58978af70daf6a
naotakak <naotakakinoshita@gmail.com>
2018-05-01 13:06:23 -0400

    working???

--- thing.py
+++ thing.py
@@ -7,10 +7,16 @@ s = s.replace(",", "").replace(".", "").replace("?", "").replace("!", "").replac
 split = s.split(' ')
 
 def singleFreq(book, word):
-    return book.count(" " + word + " ")
-
+    #return book.count(" " + word + " ")
+    book = book.split()
+    return len([w for w in book if w == word])
+    
 def totalFreq(book, words):
     return reduce(lambda i, x: i + x, [singleFreq(book, word) for word in words])
 
 def mostFreq(book):
     return reduce((lambda x, y: x if singleFreq(book, x) > singleFreq(book, y) else y), book.split(" "))
+
+print("singleFreq(s, 'Marian') " + str(singleFreq(s, "Marian")))
+print("totalFreq(s, ['Marian', 'Laura']) " + str(totalFreq(s, ["Marian", "Laura"])))
+#print("singleFreq(s, 'Marian') " + str(singleFreq(s, "Marian")))
